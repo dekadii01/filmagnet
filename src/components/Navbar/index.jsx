e3b8b1f8ffb2cd9bd6f6fd39f500171e6d3323ec
Adi Pramana <109329655+dekadii01@users.noreply.github.com>
2024-09-07 17:49:34 +0800

    delete class max width

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -31,7 +31,7 @@ const Navbar = ({ setResults, setQuerySearch }) => {
 
   return (
     <nav className="bg-transparent">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto py-4">
         <Link to={"/"} className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={LogoFilmagnet} className="h-8" alt="Filmagnet Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-homenaje">Filmagnet</span>
